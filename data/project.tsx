4fa5d45e5a761a2870a687e3ad4f074a26916b77
Kim Lasse Massesson <kima4@elev.tec.dk>
2026-03-16 12:10:25 +0100

    projekter

--- data/project.tsx
+++ data/project.tsx
@@ -21,4 +21,25 @@ export const myProjects: MyProject[] = [
     image: 'gnist.png',
     tools: 'HTML | CSS | JavaScript',
   },
+    {
+    name: 'Kf2026',
+    link: 'https://kf2026.dk/',
+    description: 'Kf2026, Koorporativt Fællesskab, en foreningsside, der stadig er under udvikling',
+    image: 'kf2026.png',
+    tools: 'HTML | CSS | JavaScript',
+  },
+    {
+    name: 'Eleutheria',
+    link: 'https://eleutheria.dk/',
+    description: 'Eleutheria, en side under Koorporativt fællesskab, der er under udvikling og som omhandler ADHD',
+    image: 'eleutheria.png',
+    tools: 'HTML | CSS | JavaScript',
+  },
+    {
+    name: 'Novocento',
+    link: 'https://novocento.dk/',
+    description: 'Min egen firmahjemmeside. En side, som jeg også bruger til at teste ting af på. Lavet i WordPress på kort tid og med login til medlemsside.',
+    image: 'novocento.png',
+    tools: 'WordPress | MySql | Php',
+  },
 ]
